545bf656e0d98f7b17925b05ada327cb552b928c
paulhoang-hee <paul.hoang@hee.nhs.uk>
2017-12-13 08:49:41 +0000

    javadoc text change

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -6,8 +6,8 @@ import com.example.service.PriceService;
 import java.io.IOException;
 
 /**
- * TASK: Read the prices from the file prices.csv on the classpath (resources directory) and sorts them in descending
- * order and outputs the result to the command line
+ * TASK: Read the prices from the file prices.csv on the classpath (resources directory) and sort them in descending
+ * order then output the result to the command line, one price per line
  *
  * Note: To kick things off, use the following to read the file
 
