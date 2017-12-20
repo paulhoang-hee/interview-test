6bb61f58c52a52a63decbfb0da42c4054edfa608
paulhoang-hee <paul.hoang@hee.nhs.uk>
2017-12-20 15:21:31 +0000

    update javadoc

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -9,10 +9,14 @@ import java.io.IOException;
  * TASK: Read the prices from the file prices.csv on the classpath (resources directory) and sort them in descending
  * order then output the result to the command line, one price per line
  *
- * Note: To kick things off, use the following to read the file
+ * Note: To kick things off, use the following to read the file (You don't need to use this but its just to get the
+         ball rolling)
 
  InputStream inputStream = Application.class.getClassLoader().getResourceAsStream("prices.csv");
  BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+ * Note 2: You have full autononmy here. Add any libraries you need, google as much as you want, ask any questions
+           to clear up any unknowns. Just code as if you are at work, delivering high quality code.
 
  */
 public class Application {
